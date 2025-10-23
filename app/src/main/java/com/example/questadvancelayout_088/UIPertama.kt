a3084c8c010abca17aa676ad7b059836bce08c13
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-10-23 12:21:08 +0700

    menambahkan Box

--- app/src/main/java/com/example/questadvancelayout_088/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_088/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.questadvancelayout_088
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -70,6 +71,9 @@ fun ActivitesPertama(modifier: Modifier){
 
 
     }
+    Box(
+
+    )
 }
 
 
