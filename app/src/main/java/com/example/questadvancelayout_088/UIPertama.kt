d776268c381e278a28850d89f7c2ce9a2f9f0b63
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2025-10-23 12:00:18 +0700

    menambahkan Image pada Bow

--- app/src/main/java/com/example/questadvancelayout_088/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_088/UIPertama.kt
@@ -1,11 +1,13 @@
 package com.example.questadvancelayout_088
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -13,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -35,11 +38,23 @@ fun ActivitesPertama(modifier: Modifier){
             .padding(12.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
-            )) {
-
+            )){
+            Bow() {
+                val gambar = painterResource(R.drawable.Logo_UMY)
+                Image(
+                    painter = gambar,
+                    contentDescription  = null,
+                    modifier = Modifier.size(100.dp).padding(5.dp)
+                )
+            }
         }
 
 
     }
+}
+
+@Composable
+fun Bow(content: @Composable () -> Unit) {
+    TODO("Not yet implemented")
 }
 
